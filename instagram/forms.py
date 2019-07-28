f18f95669d532cfcf6c8abd88d8701d5ce285c5b
Omtatah <murtallahomtatah@gmail.com>
2019-07-28 15:23:12 +0300

    try to debug

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,27 +1,24 @@
-from .models import Profile, Image, Comment, Like
-from django.contrib.auth.models import User
 from django import forms
-
+from .models import Profile,Image,Comment,Like
+from django.contrib.auth.models import User
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['followers', 'following', 'user']
-
+        exclude = ['followers','following','user']
 
 class PostImage(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['owner', 'post_date', 'profile']
-
+        exclude = ['owner','post_date','profile']
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        exclude = ['user', 'image', 'posted_on']
-
+        exclude = ['user','image','posted_on']
 
 class UpdateImage(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['image', 'owner', 'profile', 'post_date']
+        exclude = ['image','owner','profile','post_date']
+
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,19 +1,15 @@
-from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Image, Like, Comment
 from .models import Profile
-
+from django.test import TestCase
+from .models import Image,Like,Comment
 
 class Intagram_TestCases(TestCase):
     def setUp(self):
-        self.user1 = User(id=1, username='James',
-                          email='jamesone12@gmail.com', password='ppppppp')
+        self.user1= User(id=1,username='James',email='jamesone12@gmail.com',password='ppppppp')
         self.user1.save()
-        self.profile = Profile(
-            user_id=1, bio='i love cheese', profile_pic='images/food.jpg')
+        self.profile = Profile(user_id=1,bio='i love cheese',profile_pic='images/food.jpg')
         self.profile.save_profile()
-        self.my_image = Image(id=1, caption='eating is awesome', owner=self.user1,
-                              profile=self.profile, image='media/get.jpg', image_name='muk')
+        self.my_image = Image(id=1,caption='eating is awesome', owner=self.user1,profile=self.profile,image='media/get.jpg',image_name='muk')
         self.my_image.save_image()
 
     def tearDown(self):
@@ -22,14 +18,14 @@ class Intagram_TestCases(TestCase):
         Image.objects.all().delete()
 
     def test_is_instance(self):
-        self.assertTrue(isinstance(self.user1, User))
-        self.assertTrue(isinstance(self.profile, Profile))
-        self.assertTrue(isinstance(self.my_image, Image))
+        self.assertTrue(isinstance(self.user1,User))
+        self.assertTrue(isinstance(self.profile,Profile))
+        self.assertTrue(isinstance(self.my_image,Image))
 
     def test_save_method(self):
         self.my_image.save_image()
         all_objects = Image.objects.all()
-        self.assertTrue(len(all_objects) > 0)
+        self.assertTrue(len(all_objects)>0)
 
     def test_delete_method(self):
         self.my_image.save_image()
@@ -38,13 +34,15 @@ class Intagram_TestCases(TestCase):
         all_objects = Image.objects.all()
         self.assertTrue(len(all_objects) == 0)
 
+
     def test_get_image_by_id(self):
         self.my_image.save_image()
         image = Image.get_image_by_id(1)
-        self.assertEqual(image.id, 1)
+        self.assertEqual(image.id,1)
+
 
     def test_update_single_image(self):
         self.my_image.save_image()
-        filtered_object = Image.update_image('muk', 'Greener')
+        filtered_object =Image.update_image('muk','Greener')
         updated = Image.objects.get(image_name='Greener')
-        self.assertEqual(updated.image_name, 'Greener')
+        self.assertEqual(updated.image_name,'Greener')
