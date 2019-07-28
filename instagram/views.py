7b4c0da118c9e54cfe1cc21e609075c352f1c67a
Omtatah <murtallahomtatah@gmail.com>
2019-07-28 12:48:35 +0300

    define the profile view

--- instagram/views.py
+++ instagram/views.py
@@ -17,3 +17,27 @@ def home(request):
     likes = Like.objects.all().count()
     
     return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if current_user.id == user.id:
+        images = Image.objects.filter(owner_id=id)
+        current_user = request.user
+        user = User.objects.get(id=id)
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            return redirect(update_profile,current_user.id)
+    else: 
+        try:
+            profile = Profile.objects.get(user_id=id)
+        except ObjectDoesNotExist:
+            
+            return redirect(no_profile,id)      
+            
+    return render(request,'profile/profile.html',{'user':user,'profile':profile,'images':images,'current_user':current_user})
+
+
+
