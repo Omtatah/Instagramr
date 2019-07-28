b0fcda2798cd8146d755f533dea4dc017df98a41
Omtatah <murtallahomtatah@gmail.com>
2019-07-28 12:45:49 +0300

    define the index template

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    images = Image.objects.all().order_by('-post_date')
+    users = User.objects.all()  
+    current = request.user
+    likes = Like.objects.all().count()
+    
+    return render(request, 'index.html',{"images":images,'users':users,'current':current,"likes":likes})
